fd3a09a8f69e2fbb4ddc094b5b6abd1b49682a0c
Henry Pulver <henrypulver@hotmail.co.uk>
2022-11-01 15:54:14 +0000

    Add basic tests

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,9 +1,26 @@
-import pytest
-
-from game_mechanics import Env
+from delta_connect4.game_mechanics import Connect4Env
 
 
-def test_env():
-    """Take me out in real games."""
-    with pytest.raises(NotImplementedError):
-        env = Env()
+def test_init():
+    env = Connect4Env()
+    assert env is not None
+
+
+def test_reset():
+    env = Connect4Env()
+    state, reward, done, info = env.reset()
+    assert state is not None
+    assert reward == 0
+    assert not done
+    assert "player_to_take_next_move" in info
+
+
+def test_step():
+    env = Connect4Env()
+    env.reset()
+    state, reward, done, info = env.step(0)
+    assert state is not None
+    assert reward == 0
+    assert not done
+    assert "player_to_take_next_move" in info
+    assert state[5][0] == 1
