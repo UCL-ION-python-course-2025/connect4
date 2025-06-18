0f5d1ae13a0e9ad5e8640a9a6f53a89760e8e075
neuromantic99 <jrowlandm@gmail.com>
2025-06-18 12:20:57 +0100

    .replit files

--- init_replit.sh
+++ init_replit.sh
@@ -3,8 +3,7 @@ function getDeltaFolder()
     find . -name "*delta_*" -type d
 }
 
+pip install -r requirements.txt
 folder=$(getDeltaFolder)
 mv $folder/* $folder/.* .
 rm -r $folder/
-pip install -r requirements.txt
-pip3 install torch==1.8.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
